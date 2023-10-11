218a5ee5bf06f9e00e1cd6df20feea96c7529810
Andres Rosas <andresrosas@utez.edu.mx>
2023-10-11 21:39:21 +0000

    tercera funcion de control.js

--- back/personas/control.js
+++ back/personas/control.js
@@ -2,5 +2,8 @@ const funcionUno = (x) => {
     console.log("hola: este es X: ", x);
 }
 const funcionDos = (x,y) => {
-    console.log("X y Y:");
+    console.log("X y Y");
+}
+const funcionTres = (x,y,z) => {
+    console.log("X, Y y Z");
 }
